f922479a29fb2b0e21d89cb6864ee9812c3c5ce7
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-12-27 11:24:59 +0530

    Fix axios baseURL configuration for production environment

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://todo-app-pern-3ddo.onrender.com/api",
   withCredentials: true,
 });
