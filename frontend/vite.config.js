9b25cdefb65d044bc3464e929625805daf95377b
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-12-27 18:19:02 +0530

    Update manifest and remove unused web app manifest files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/pwa-192.png",
-            sizes: "192x192",
+            src: "/favicon-96x96.png",
+            sizes: "96x96",
             type: "image/png",
           },
           {
-            src: "/pwa-512.png",
+            src: "/favicon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
