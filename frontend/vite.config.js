aca0343daf1ad3a0ec48260ad036d6ac7f1c0d8e
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-12-27 18:36:44 +0530

    Update web app manifest and add new icons
    - Replaced favicon references in manifest.json and vite.config.js with new web app icons (192x192 and 512x512).
    - Added a new site.webmanifest file for improved PWA support.
    - Introduced new PNG files for web app icons.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,8 +19,13 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/favicon-96x96.png",
-            sizes: "96x96",
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
             type: "image/png",
           },
         ],
