8e19fd171691b89130747af9dbc286ca596f14a6
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-12-29 16:27:29 +0530

    Fix axios base URL for production environment

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://vercel.com/abhisheks-projects-e468d787/todo-app-pern/9qRcp8gE1RDLmn7xRy7SRixKRP3N/api",
+      : "https://todo-app-pern.vercel.app/api",
   withCredentials: true,
 });
