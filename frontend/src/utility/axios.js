7e4d65472d3661d7d9b7f1ae7ee02f37502c247c
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-12-29 11:34:04 +0530

    Update axios base URL for production environment

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://todo-app-pern-3ddo.onrender.com/api",
+      : "https://vercel.com/abhisheks-projects-e468d787/todo-app-pern/9qRcp8gE1RDLmn7xRy7SRixKRP3N/api",
   withCredentials: true,
 });
